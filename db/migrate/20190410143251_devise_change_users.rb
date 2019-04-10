ea6a8c2f9416d1a7909a0e22b8b5d7cadbe03af7
ahmed bahaa <a.bahaarefaii@gmail.com>
2019-04-10 17:10:37 +0200

    change migration date

---
+++ db/migrate/20190410143251_devise_change_users.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+class DeviseChangeUsers < ActiveRecord::Migration[5.2]
+    def change
+      change_table :users do |t|
+        t.string :name
+        t.string :friends
+        t.string :groups
+        t.string :orders
+      end
+    end
+end
