555d4cb24a2267ebebd856bebb4f9e67bbebfee1
AnasMuhammed2020 <mr_seleman@icloud.com>
2019-04-10 15:26:28 +0200

    Adding devise and User controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_09_182952) do
-
-  create_table "orders", force: :cascade do |t|
-    t.string "order_status"
-    t.string "order_type"
-    t.string "restaurant"
-    t.string "menu_image"
-    t.string "joined_users"
-    t.string "invited_users"
-    t.string "order_items"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "user_id"
-    t.index ["user_id"], name: "index_orders_on_user_id", unique: true
-ActiveRecord::Schema.define(version: 2019_04_09_180903) do
+ActiveRecord::Schema.define(version: 2019_04_10_124906) do
 
   create_table "items", force: :cascade do |t|
     t.integer "item_id"
@@ -38,6 +24,36 @@ ActiveRecord::Schema.define(version: 2019_04_09_180903) do
     t.integer "user_id"
     t.index ["order_id"], name: "index_items_on_order_id", unique: true
     t.index ["user_id"], name: "index_items_on_user_id", unique: true
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "order_status"
+    t.string "order_type"
+    t.string "restaurant"
+    t.string "menu_image"
+    t.string "joined_users"
+    t.string "invited_users"
+    t.string "order_items"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_orders_on_user_id", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "friends"
+    t.string "groups"
+    t.string "orders"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
