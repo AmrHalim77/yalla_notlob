cc2973e84af025f4f5bb2db49da4a58751f4ac92
David Poules <david_fci@outlook.com>
2019-04-13 21:00:51 +0200

    last commit from david

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   resources :friendships
   post '/friendships/create', to: 'friendships#create'
   post '/friendships/destroy', to: 'friendships#destroy'
-=======
   resources :groups
   # get 'groups/index'
   # get 'groups/show'
@@ -12,7 +10,6 @@ Rails.application.routes.draw do
   # get 'groups/destroy'
 
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
->>>>>>> 033e0c0e25d2933675e70f741f0120408966bdd9
   get 'order/index'
   get 'order/toitem'
   get 'order/show'
